40a801e34504fd32e97c4ea86a304b5e6b8e93af
reza <reza_shahriari@yahoo.ca>
2019-08-14 00:39:00 -0400

    added new label compare

--- server/routes/boundingBox/index.js
+++ server/routes/boundingBox/index.js
@@ -170,9 +170,7 @@ router.get('/listFilteredBoundingBoxes', function(req,res){
 		for(let i=0; i<boxes.length; i++){
 			//for each box we should put into reply with proper format
 			for(let j=0; j<labels.length; j++){
-				console.log(JSON.stringify(boxes[i].label))
-				console.log(JSON.stringify(labels[j]))
-				if(JSON.stringify(boxes[i].label)==JSON.stringify(labels[j])){
+				if( _.isEqual(_.sortBy(boxes[i].label), _.sortBy(labels[j])) ){
 					reply.push(
 						{
 							boundingBoxID: boxes[i].bounding_box_id,
