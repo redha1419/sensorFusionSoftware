1d2356db20b08498eeff53bb1c0069663d1ed1b1
reza <reza_shahriari@yahoo.ca>
2019-08-14 00:45:01 -0400

    more logging

--- server/routes/boundingBox/index.js
+++ server/routes/boundingBox/index.js
@@ -170,6 +170,8 @@ router.get('/listFilteredBoundingBoxes', function(req,res){
 		for(let i=0; i<boxes.length; i++){
 			//for each box we should put into reply with proper format
 			for(let j=0; j<labels.length; j++){
+				console.log("BOX: "+boxes[i].label)
+				console.log("LABEL: "+labels[j])
 				let xor = _.xor(boxes[i].label, labels[j])
 				console.log(xor)
 				if(xor.length === 0){
