6ad40fd87d9fd024d8811cfb806c95376b02f533
reza <reza_shahriari@yahoo.ca>
2019-08-16 11:12:39 -0400

    fixed typo

--- server/routes/sensor/index.js
+++ server/routes/sensor/index.js
@@ -10,11 +10,9 @@ const projectHelper = require('../libs/helperFunctions.js')(knex);
 const authenticaton = require('../libs/users.js')(knex);
 	
 function createSensorInstance(req, ID){
-	console.log('craeteing a sensor object')
-	console.log(req.body)
 	var sensor = {
 		'sensor_id': uuidv4(),
-		'sensor_type': req.body.sensorReference,
+		'sensor_type': req.body.sensorType,
 		'sensor_name': req.body.sensorName,
 		'sensor_mode': req.body.sensorMode
 	};
