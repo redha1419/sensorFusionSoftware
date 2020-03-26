95f7b753ef06eada15e7470fe0a311bbe8a76337
reza <reza_shahriari@yahoo.ca>
2020-03-26 19:28:06 -0400

    fri

--- server/routes/project/index.js
+++ server/routes/project/index.js
@@ -279,7 +279,7 @@ router.get('/listProjects', function(req,res){
 	.join('sensors', 'sensors.project_id', '=', 'projects.project_id')
 	.select('*')
 	.count({num_of_users: 'users_projects.user_id'})
-	.groupBy('users_projects.project_id', 'users_projects.id') //'projects.id','users_projects.id','sensors.project_id','sensors.sensor_id','sensors.sensor_type', 'sensors.sensor_name', 'sensors.sensor_name', 'sensors.sensor_mode', 'sensors.data_path','sensors.id')
+	.groupBy('users_projects.project_id', 'users_projects.id','projects.id','users_projects.id','sensors.project_id','sensors.sensor_id','sensors.sensor_type', 'sensors.sensor_name', 'sensors.sensor_name', 'sensors.sensor_mode', 'sensors.data_path','sensors.id')
 	.count({num_of_sensors: 'sensors.sensor_id'})
 	.groupBy('sensors.project_id')
 	.then(projects=>{
