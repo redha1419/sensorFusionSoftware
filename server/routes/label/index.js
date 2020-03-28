f7ce3ac6a3a7546a7b28f56bccc36d4c8bb23fcd
reza <reza_shahriari@yahoo.ca>
2020-03-28 19:55:44 -0400

    added colors to /label

--- server/routes/label/index.js
+++ server/routes/label/index.js
@@ -55,34 +55,54 @@ function getLabels(projectID, obj){
     return labels_to_insert;
 }
 
+function getColors(projectID, obj){
+    let group_name = obj.group_name;
+    let nodes = obj.nodes;
+    let colors_to_insert = [];
+    for(let i=0; i<nodes.length; i++){
+        colors_to_insert.push(
+            {
+                node_ID: nodes[i].current_node,
+                node_colour: nodes[i].node_colour
+            }
+        )
+    }
+    return {group_name, nodes: colors_to_insert};
+}
+
 router.post('/label', function(req,res){
     let projectID = req.body.projectID;
     let given_labels = req.body.class_names;
+
     let labels_to_insert = [];
-    
+
+    let colors_to_insert = {};
+    colors_to_insert.objects = [];    
 
     for(let i=0; i<given_labels.objects.length; i++){
         labels_to_insert.concat(getLabels(projectID, given_labels.objects[i]));
     }
+
+    for(let i=0; i<given_labels.objects.length; i++){
+        colors_to_insert.objects.push(getColors(projectID, given_labels.objects[i]));
+    }
     
     console.log(labels_to_insert);
+    console.log(colors_to_insert);
 
     knex('labels')
     .insert(labels_to_insert)
     .returning(['*'])
     .then(labels => {
         // inserted labels
-        /*
         knex('users_projects')
         .where({
             project_id: projectID
         })
-        .update({label_colors: req.body.class_names})
+        .update({label_colors: colors_to_insert})
         .then(()=>{
-        */
-            //console.log(labels)
             res.status(200).json({message: "Succesfully Inserted labels"})
-       // })
+        })
     })
     .catch(err=>{
         res.status(500).json({message: err.message, stack:err.stack});
